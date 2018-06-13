c98e9ba33ac619b31e23eaa4a955278e89b3e8a2
MuhammadUmarWaheed <umarwaheedlog@gmail.com>
2018-06-13 10:10:42 +0500

    #409 SaveDashboard Business Logic Changed

--- diamond-emporium-crm/html/module/SaveDashboard/src/SaveDashboard/Model/SaveDashboardTable.php
+++ diamond-emporium-crm/html/module/SaveDashboard/src/SaveDashboard/Model/SaveDashboardTable.php
@@ -1,276 +1,285 @@
-  <?php
-  namespace SaveDashboard\Model;
+<?php
+namespace SaveDashboard\Model;
 
-  use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\TableGateway\TableGateway;
 
 
-  class SaveDashboardTable
-  {
-  	protected $tableGateway;
-  	
-  	public function __construct(TableGateway $tableGateway)
-  	{
-  		$this->tableGateway = $tableGateway;
-  	}
-  	
-  	public function executeQuery($select){
-  		$adapter = $this->tableGateway->getAdapter();
-  		$statement = $adapter->createStatement();
-  		$select->prepareStatement($adapter, $statement);
-  		$resultSet = new \Zend\Db\ResultSet\ResultSet();
-  		$resultSet->initialize($statement->execute());
-  		return $resultSet;
-  	}
-  	
-  	/*public function fetchAll()
-  	{
-  		try{
-  			
-  			$select = new \Zend\Db\Sql\Select();
-  			$select->from('de_leads')->columns(array('lead_id'));
-  			
-  			//FullName  From Table "de_users"
-  			$fullname = new \Zend\Db\Sql\Expression(
-  				'CONCAT(u.first_name, \' \', u.last_name)'
-  			);
-                          //Image  From Table "de_users"
-                          $image = new \Zend\Db\Sql\Expression(
-  				'u.image'
-  			);
-  			$select = new \Zend\Db\Sql\Select();
-  			$select->from(array('l' => 'de_leads'))
-  				   ->columns(array(
-  				   		'lead_id', 'title', 'first_name', 'last_name', 'email', 'mobile', 'budget', 'priority', 'created_date','lead_owner'				   		
-  				   ))				   
-  				   ->join(array('u' => 'de_users'), 'l.lead_owner = u.user_id', array('lead_owner_fullname' => $fullname , 'lead_owner_image' => $image ), 'left');
+class SaveDashboardTable
+{
+	protected $tableGateway;
+	
+	public function __construct(TableGateway $tableGateway)
+	{
+		$this->tableGateway = $tableGateway;
+	}
+	
+	public function executeQuery($select){
+		$adapter = $this->tableGateway->getAdapter();
+		$statement = $adapter->createStatement();
+		$select->prepareStatement($adapter, $statement);
+		$resultSet = new \Zend\Db\ResultSet\ResultSet();
+		$resultSet->initialize($statement->execute());
+		return $resultSet;
+	}
+	
+	/*public function fetchAll()
+	{
+		try{
+			
+			$select = new \Zend\Db\Sql\Select();
+			$select->from('de_leads')->columns(array('lead_id'));
+			
+			//FullName  From Table "de_users"
+			$fullname = new \Zend\Db\Sql\Expression(
+				'CONCAT(u.first_name, \' \', u.last_name)'
+			);
+                        //Image  From Table "de_users"
+                        $image = new \Zend\Db\Sql\Expression(
+				'u.image'
+			);
+			$select = new \Zend\Db\Sql\Select();
+			$select->from(array('l' => 'de_leads'))
+				   ->columns(array(
+				   		'lead_id', 'title', 'first_name', 'last_name', 'email', 'mobile', 'budget', 'priority', 'created_date','lead_owner'				   		
+				   ))				   
+				   ->join(array('u' => 'de_users'), 'l.lead_owner = u.user_id', array('lead_owner_fullname' => $fullname , 'lead_owner_image' => $image ), 'left');
 
-  			$data = $this->executeQuery($select);
-  			//$result['TotalRows'] = count($counter);
-  			//$result['Rows'] = $data->toArray();
-                          $result = $data->toArray();
-  			
-                          //ArrayGrouped BasedOnId
-                        $groups = array();
-                     foreach ($result as $item) {
-                         $key = $item['lead_owner'];
-                         if (!isset($groups[$key])) {
-                             $groups[$key] = array(
-                            'items' => array($item),
-                            'count' => 1,
-                               );
-                         } else {
-                          $groups[$key]['items'][] = $item;
-                          $groups[$key]['count'] += 1;
-                         }
-                        }
-                          //EndArrayGrouped Based on Id
-                          
-  			return $groups;
-  		}catch(\Exception $e){
-  			\De\Log::logApplicationInfo ( "Caught Exception: " . $e->getMessage () . ' -- File: ' . __FILE__ . ' Line: ' . __LINE__ );
-  		}
-  	}*/
+			$data = $this->executeQuery($select);
+			//$result['TotalRows'] = count($counter);
+			//$result['Rows'] = $data->toArray();
+                        $result = $data->toArray();
+			
+                        //ArrayGrouped BasedOnId
+                      $groups = array();
+                   foreach ($result as $item) {
+                       $key = $item['lead_owner'];
+                       if (!isset($groups[$key])) {
+                           $groups[$key] = array(
+                          'items' => array($item),
+                          'count' => 1,
+                             );
+                       } else {
+                        $groups[$key]['items'][] = $item;
+                        $groups[$key]['count'] += 1;
+                       }
+                      }
+                        //EndArrayGrouped Based on Id
+                        
+			return $groups;
+		}catch(\Exception $e){
+			\De\Log::logApplicationInfo ( "Caught Exception: " . $e->getMessage () . ' -- File: ' . __FILE__ . ' Line: ' . __LINE__ );
+		}
+	}*/
 
-          
-         
-          //******************************************DashboardCode****************************************************//
+        
+       
+        //******************************************DashboardCode****************************************************//
 
-     
-          
-  //***************************************CoreFunction******************************************//
    
+        
+//***************************************CoreFunction******************************************//
+ 
 
+    // Insert | Update Data
       // Insert | Update Data
-        // Insert | Update Data
-      public function saveDashboard($data , $where = null)
-      {
-          try
-          {
+    public function saveDashboard($data , $where = null)
+    {
+        try
+        {
+          /*Product Short Code*/            
+            $product_name = explode(" ", $data['product']);           
+            $lastname_product = array_pop($product_name);
+            $firstname_product = implode(" ", $product_name);
+            $first_letter = $firstname_product[0];
+            $last_letter = $lastname_product[0];
+            $product_shortcode = $first_letter.$last_letter;
+            $data['product_shortcode'] = $product_shortcode;
+          /*Product Short Code*/
+          /*AssignUsUserShortCode*/
+            $assigni_name = explode(" ", $data['assign_to']);
+            $lastname_assigni_name = array_pop($assigni_name);
+            $firstname_assigni_name = implode(" ", $assigni_name);
+            $first_letter_assigni_name = $firstname_assigni_name[0];
+            $last_letter_assigni_name = $lastname_assigni_name[0];
+            $assign_shortcode = $first_letter_assigni_name.$last_letter_assigni_name;
+            $data['assignto_shortcode'] = $assign_shortcode;
+          /*AssignUsUserShortCode*/
+            $AssignInUserId = $data['assign_id'];
+            unset($data['assign_id']);
+            $data['assign_to_UserId'] = $AssignInUserId;
+	    $data['create_date'] = date('Y-m-d H:i:s');     
+            $leadId = $data['lead_id'];
+            unset($data['lead_id']);
+            if(empty($leadId))
+             {
+              $data['lead_status'] = 'Open';
+           
+            if(empty($data['booking_room']))
+             {
+               if(empty($data['booking_date']))
+               {
+                   unset($data['booking_room']);
+                   $data['booking_date'] = date('Y-m-d'); 
+                   $data['user_booking_date'] = 1;
+               }
+             }
+           else
+           {
+               $data['user_booking_date'] = 0;
+           }
           
-         $leadId = $data['lead_id'];
-         unset($data['lead_id']);
-         if(empty($leadId))
-          {
-            $data['lead_status'] = 'Open'; 
-            unset($data['booking_room']);
-            $select = new \Zend\Db\Sql\Select();
-  			$select->from('de_userdetail')->columns(array('id'));
+           
             
-            $select = new \Zend\Db\Sql\Select();
-  			$select->from(array('l' => 'de_userdetail'))
-  				   ->columns(array('id'));	
-            
+           
+           
+           //unset($data['booking_room']);
+           
+           /*$select = new \Zend\Db\Sql\Select();
+	   $select->from('de_userdetail')->columns(array('id'));
+          
+           $select = new \Zend\Db\Sql\Select();
+	   $select->from(array('l' => 'de_userdetail'))->columns(array('id'));
+				   	
+           
+           $booking_date = $data['booking_date'];
+           
+           $booking_time = $data['booking_time'];
+           $booking_timezone = $data['booking_timezone'];
+           $select->where(array('l.booking_date = ?' => $booking_date ,'l.booking_time = ?' => $booking_time ,'l.booking_timezone = ?' => $booking_timezone ));
+           $exec_data = $this->executeQuery($select);
+           $counter = count($exec_data);
+	   
+	   if($counter == 0)
+              {
+             //TODO HANDLER booking_room  set to 1
+             $data['booking_room'] = 1;
+              }
+           else if($counter == 1)
+              {
+             //TODO HANDLER booking_room set to 2
+             $data['booking_room'] = 2;
+              }
+           else if($counter == 2)
+             {
+             //TODO HANDLER booking_room set to 3
+             $data['booking_room'] = 3;
+             }
+          else
+             {
+            //TODO HANDLER    return 0 or exit;
+            return " Room Full Pleae Find Another Nearby Hotel";
+             }*/
+         
+           return $this->tableGateway->insert($data);
+   
+           }       
+        
+          else {
+  
+          
+          
+          unset($data['booking_room']);
+          $select = new \Zend\Db\Sql\Select();
+	  $select->from('de_userdetail')->columns(array('id'));
+          
+          $select = new \Zend\Db\Sql\Select();
+			$select->from(array('l' => 'de_userdetail'))
+				   ->columns(array('id','booking_date','booking_time','booking_timezone'));	
+          
           $booking_date = $data['booking_date'];
           $booking_time = $data['booking_time'];
           $booking_timezone = $data['booking_timezone'];
-          //Start-IF Booking Date is null or empty then add current date
-          if($booking_date == ''){
-              
-              $data['booking_date'] = date('Y-m-d');
-              $date['user_booking_date'] = 0;
-          }
-          else
-          {
-              $date['user_booking_date'] = 1;
-          }
-          //End-IF Booking Date is null or empty then add current date
-          
+        
+        
+         /*if(!empty($booking_date && $booking_time && $booking_timezone))              
+             {*/
+	 $select->where(array('l.booking_date = ?' => $booking_date ,'l.booking_time = ?' => $booking_time ,'l.booking_timezone = ?' => $booking_timezone ));
+	     /*}*/
+         $exec_data = $this->executeQuery($select);
+         $data_booking = $exec_data->toArray();
+         $counter = count($exec_data);
          
-  	$select->where(array('l.booking_date = ?' => $booking_date ,'l.booking_time = ?' => $booking_time ,'l.booking_timezone = ?' => $booking_timezone ));	
-          $exec_data = $this->executeQuery($select);
-          $counter = count($exec_data);
-          //Start-Condition is Apply when BookingTime Is Not Null Or Empty
-            if($booking_time != '')
-           {
-              if($counter == 0)
-             {
-               //TODO HANDLER booking_room  set to 1
-               $data['booking_room'] = 1;
-             }
-             else if($counter == 1)
-             {
-               //TODO HANDLER booking_room set to 2
-               $data['booking_room'] = 2;
-             }
-             else if($counter == 2)
-             {
-               //TODO HANDLER booking_room set to 3
-               $data['booking_room'] = 3;
-             }
-             else
-             {
-              //TODO HANDLER    return 0 or exit;
-              return " Room Full Pleae Find Another Nearby Hotel";
-             }
-                
-            }
-            //End-Condition is Apply when BookingTime Is Not Null Or Empty
-                   
-            //Insert The Record Into The Database
-                   return $this->tableGateway->insert($data);
-                   
-          }
-            else {
-    
-            
-            
-            unset($data['booking_room']);
-            $select = new \Zend\Db\Sql\Select();
-  	  $select->from('de_userdetail')->columns(array('id'));
-            
-            $select = new \Zend\Db\Sql\Select();
-  			$select->from(array('l' => 'de_userdetail'))
-  				   ->columns(array('id','booking_date','booking_time','booking_timezone'));	
-            
-            $booking_date = $data['booking_date'];
-            $booking_time = $data['booking_time'];
-            $booking_timezone = $data['booking_timezone'];
+         $data_bookingDate = $data_booking[0]['booking_date'];
+         $data_bookingTime = $data_booking[0]['booking_time'];
+         $data_bookingTimeZone = $data_booking[0]['booking_timezone'];
+         
+         if($data_bookingDate ==  $booking_date  && $data_bookingTime == $booking_time && $data_bookingTimeZone == $booking_timezone  )
+         {
           
-           if($booking_date == ''){
-              
-              $data['booking_date'] = date('Y-m-d');
-              $date['user_booking_date'] = 0;
-          }
-          else
-          {
-              $date['user_booking_date'] = 1;
-          }
-           /*if(!empty($booking_date && $booking_time && $booking_timezone))              
-               {*/
-  	 $select->where(array('l.booking_date = ?' => $booking_date ,'l.booking_time = ?' => $booking_time ,'l.booking_timezone = ?' => $booking_timezone ));
-  	     /*}*/
-           $exec_data = $this->executeQuery($select);
-           $data_booking = $exec_data->toArray();
-           $counter = count($exec_data);
-           
-           $data_bookingDate = $data_booking[0]['booking_date'];
-           $data_bookingTime = $data_booking[0]['booking_time'];
-           $data_bookingTimeZone = $data_booking[0]['booking_timezone'];
-           
-           if($data_bookingDate ==  $booking_date  && $data_bookingTime == $booking_time && $data_bookingTimeZone == $booking_timezone  )
-           {
-            
-               if($where)
-  		  return $this->tableGateway->update($data, $where);
-  		else
-  		 return $this->tableGateway->update($data, array('id' => $leadId));
-               
-           }
-          else {
-           if($booking_time != '')
-           {
-              if($counter == 0)
-             {
-               //TODO HANDLER booking_room  set to 1
-               $data['booking_room'] = 1;
-             }
-             else if($counter == 1)
-             {
-               //TODO HANDLER booking_room set to 2
-               $data['booking_room'] = 2;
-             }
-             else if($counter == 2)
-             {
-               //TODO HANDLER booking_room set to 3
-               $data['booking_room'] = 3;
-             }
-             else
-             {
-              //TODO HANDLER    return 0 or exit;
-              return " Room Full Pleae Find Another Nearby Hotel";
-             }
-                
-            }
-                
-  	        if($where)
-  		  return $this->tableGateway->update($data, $where);
-  		else
-  		 return $this->tableGateway->update($data, array('id' => $leadId));
-              
-          }
-               
-           
-  			
-                  
-               }
-  				   		              
-            //return $this->tableGateway->insert($data);
-
-            
-          }
-       	 catch(\Exception $e){
-  			\De\Log::logApplicationInfo ( "Caught Exception: " . $e->getMessage () . ' -- File: ' . __FILE__ . ' Line: ' . __LINE__ );
-  		}
-      }
-      
-      //Update Lead Status
-         public function updateLeadStatus($data, $where = null)
-  	     {
-           	try{
-        			$lead_id = $data['lead_id'];
-        			unset($data['lead_id']);
-        			if (empty($lead_id)) {
-        			
-                                    //Lead Not Exist
-                                    
-        			} 
-              else 
-              {
-        				if($where)
-        					return $this->tableGateway->update($data, $where);
-        				else
-        					return $this->tableGateway->update($data, array('id' => $lead_id));
-        			}
-           	}
-            catch(\Exception $e)
-            {
-        			\De\Log::logApplicationInfo ( "Caught Exception: " . $e->getMessage () . ' -- File: ' . __FILE__ . ' Line: ' . __LINE__ );
-        		}
+             if($where)
+		  return $this->tableGateway->update($data, $where);
+		else
+		 return $this->tableGateway->update($data, array('id' => $leadId));
+             
          }
-       
-      
+        else {
+            
+            if($counter == 0)
+         {
+             //TODO HANDLER booking_room  set to 1
+             $data['booking_room'] = 1;
+         }
+         else if($counter == 1)
+         {
+             //TODO HANDLER booking_room set to 2
+             $data['booking_room'] = 2;
+         }
+         else if($counter == 2)
+         {
+             //TODO HANDLER booking_room set to 3
+             $data['booking_room'] = 3;
+         }
+        else
+        {
+            //TODO HANDLER    return 0 or exit;
+            return " Room Full Pleae Find Another Nearby Hotel";
+        }
+              
+	        if($where)
+		  return $this->tableGateway->update($data, $where);
+		else
+		 return $this->tableGateway->update($data, array('id' => $leadId));
+            
+        }
+             
+         
+			
+                
+             }
+				   		              
+          //return $this->tableGateway->insert($data);
 
+          
+        }
+     	 catch(\Exception $e){
+			\De\Log::logApplicationInfo ( "Caught Exception: " . $e->getMessage () . ' -- File: ' . __FILE__ . ' Line: ' . __LINE__ );
+		}
+    }
+    
+    //Update Lead Status
+       public function updateLeadStatus($data, $where = null)
+	{
+     	try{
+			$lead_id = $data['lead_id'];
+			unset($data['lead_id']);
+			if (empty($lead_id)) {
+			
+                            //Lead Not Exist
+                            
+			} else {
+				if($where)
+					return $this->tableGateway->update($data, $where);
+				else
+					return $this->tableGateway->update($data, array('id' => $lead_id));
+			}
+     	}catch(\Exception $e){
+			\De\Log::logApplicationInfo ( "Caught Exception: " . $e->getMessage () . ' -- File: ' . __FILE__ . ' Line: ' . __LINE__ );
+		}
+     }
      
-  //***************************************CoreFunction*****************************************//    
+    
 
-  }
+   
+//***************************************CoreFunction*****************************************//    
+
+}
