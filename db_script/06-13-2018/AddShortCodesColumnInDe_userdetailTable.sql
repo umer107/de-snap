1604541855abda8f701cec32a87e4a4ad0524fc8
MuhammadUmarWaheed <umarwaheedlog@gmail.com>
2018-06-13 10:09:18 +0500

    #409 Database Script

---
+++ db_script/06-13-2018/AddShortCodesColumnInDe_userdetailTable.sql
@@ -0,0 +1,7 @@
+
+use openseedcrm;
+
+ALTER TABLE `de_userdetail` ADD `product_shortcode` VARCHAR(8) NULL AFTER `product`;
+
+
+ALTER TABLE `de_userdetail` ADD `product_shortcode` VARCHAR(8) NULL AFTER `product`;
---
+++ db_script/06-13-2018/DropExtraColumns.sql
@@ -0,0 +1,3 @@
+Use openseedcrm;
+Alter Table de_userdetail Drop column booking_timezone;
+Alter Table de_userdetail Drop column booking_duration;
