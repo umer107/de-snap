b6fd2764e9cd6751ab492a7ce3cd5962c5ac7730
MuhammadUmarWaheed <umarwaheedlog@gmail.com>
2018-11-09 15:49:44 +0500

    #512 Insert New Enterprise In How Heard

---
+++ db_script/New Requirements/11-09-2018/Insert New Enteries in How Heard.sql
@@ -0,0 +1,2 @@
+INSERT INTO `de_how_heard_lookup` (`id`, `how_heard`) VALUES ('11', 'Previous client'), ('12', 'Walk In');
+INSERT INTO `de_how_heard_lookup` (`id`, `how_heard`) VALUES ('13', 'Instagram');
